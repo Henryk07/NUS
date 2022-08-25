97b19674bced0cb2291cec21eba0b7ecf36d3fcb
Henryk Chen <45092460+Henryk07@users.noreply.github.com>
2022-08-25 20:38:38 +0800

    ANN_assignment

--- P_Filter/ESP-particleFilter-assignment/particle_filter.py
+++ P_Filter/ESP-particleFilter-assignment/particle_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.stats
 
+
 def initialize_particles(num_particles, map_limits):
     # randomly initialize the particles inside the map limits
     particles = []
@@ -24,15 +25,15 @@ def mean_pose(particles):
     #
     # for x and y, the mean position is the mean of the particle coordinates
     #
-    # for theta, we cannot simply average the angles because of the wraparound 
-    # (jump from -pi to pi). Therefore, we generate unit vectors from the 
-    # angles and calculate the angle of their average 
+    # for theta, we cannot simply average the angles because of the wraparound
+    # (jump from -pi to pi). Therefore, we generate unit vectors from the
+    # angles and calculate the angle of their average
 
     # save x and y coordinates of particles
     xs = []
     ys = []
 
-    # save unit vectors corresponding to particle orientations 
+    # save unit vectors corresponding to particle orientations
     vxs_theta = []
     vys_theta = []
 
@@ -40,20 +41,21 @@ def mean_pose(particles):
         xs.append(particle['x'])
         ys.append(particle['y'])
 
-        #make unit vector from particle orientation
+        # make unit vector from particle orientation
         vxs_theta.append(np.cos(particle['theta']))
         vys_theta.append(np.sin(particle['theta']))
 
-    #calculate average coordinates
+    # calculate average coordinates
     mean_x = np.mean(xs)
     mean_y = np.mean(ys)
     mean_theta = np.arctan2(np.mean(vys_theta), np.mean(vxs_theta))
 
     return [mean_x, mean_y, mean_theta]
 
+
 def sample_motion_model(odometry, particles):
     # Samples new particle positions, based on old positions, the odometry
-    # measurements and the motion noise 
+    # measurements and the motion noise
 
     delta_rot1 = odometry['r1']
     delta_trans = odometry['t']
@@ -61,23 +63,25 @@ def sample_motion_model(odometry, particles):
 
     # the motion noise parameters: [alpha1, alpha2, alpha3, alpha4]
     noise = [0.1, 0.1, 0.05, 0.05]
-    
+
     # standard deviations of motion noise
     sigma_delta_rot1 = noise[0] * abs(delta_rot1) + noise[1] * delta_trans
-    sigma_delta_trans = noise[2] * delta_trans + noise[3] * (abs(delta_rot1) + abs(delta_rot2))
+    sigma_delta_trans = noise[2] * delta_trans + \
+        noise[3] * (abs(delta_rot1) + abs(delta_rot2))
     sigma_delta_rot2 = noise[0] * abs(delta_rot2) + noise[1] * delta_trans
-    
+
     # generate new particle set after motion update
     new_particles = []
 
     for particle in particles:
         new_particle = dict()
-        #sample noisy motions
+        # sample noisy motions
         noisy_delta_rot1 = delta_rot1 + np.random.normal(0, sigma_delta_rot1)
-        noisy_delta_trans = delta_trans + np.random.normal(0, sigma_delta_trans)
+        noisy_delta_trans = delta_trans + \
+            np.random.normal(0, sigma_delta_trans)
         noisy_delta_rot2 = delta_rot2 + np.random.normal(0, sigma_delta_rot2)
-        
-        #calculate new particle pose
+
+        # calculate new particle pose
         new_particle['x'] = particle['x'] + \
             noisy_delta_trans * np.cos(particle['theta'] + noisy_delta_rot1)
         new_particle['y'] = particle['y'] + \
@@ -96,21 +100,22 @@ def eval_sensor_model(sensor_data, particles, landmarks):
 
     sigma_r = 0.2
 
-    #measured landmark ids and ranges
+    # measured landmark ids and ranges
     ids = sensor_data['id']
     ranges = sensor_data['range']
 
     weights = []
-    
+
     '''your code here'''
+
     '''***        ***'''
 
-
-    #normalize weights
-    normalizer = sum(weights)
-    weights = weights / normalizer
+    # normalize weights
+    #normalizer = sum(weights)
+    #weights = weights[0] / normalizer
 
     return weights
+
 
 def resample_particles(particles, weights):
     # Returns a new set of particles obtained by performing
@@ -121,6 +126,4 @@ def resample_particles(particles, weights):
     '''your code here'''
     '''***        ***'''
 
-
     return new_particles
-
